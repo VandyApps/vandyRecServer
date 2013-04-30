0b822a273ea8cc987037e8a692b005d9ef95700d
Brendan McNamara <brendan@streampiece.com>
2013-04-29 20:52:14 -0500

    Creates all tabs

--- app.js
+++ app.js
@@ -32,7 +32,16 @@ app.configure('development', function(){
 
 //routes
 app.get('/', routes.index);
+app.get('/news', routes.news);
+app.get('/hours', routes.hours);
+app.get('/groupFitness', routes.groupFitness);
+app.get('/traffic', routes.traffic);
+app.get('/intramurals', routes.intramurals);
+app.get('/programs', routes.programs);
+
 app.get('/users', user.list);
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,34 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index');
+};
+
+exports.news = function(req, res) {
+	res.render('news');
+
+};
+exports.hours = function(req, res) {
+
+	res.render('hours')
+};
+
+exports.intramurals = function(req, res) {
+
+	res.render('intramurals')
+};
+
+exports.groupFitness = function(req, res) {
+
+	res.render('groupFitness');
+};
+
+exports.traffic = function(req, res) {
+
+	res.render('traffic');
+};
+
+exports.programs = function(req, res) {
+
+	res.render('programs');
 };
