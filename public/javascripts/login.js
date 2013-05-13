0a38b1306c8126c1a2cdf1bb416850a4fed6a5e8
Brendan McNamara <brendan@streampiece.com>
2013-05-12 23:08:11 -0500

    Adds client side credential checking

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,3 +1,11 @@
-$('#submit').click(function() {
-	
+$('#loginForm').submit(function() {
+	if ($('.loginTextInput[name="username"]').attr('value') === '') {
+		$('#warning').text('You need to include a username');
+		return false;
+	} else if ($('.loginTextInput[name="password"]').attr('value') === '') {
+		$('#warning').text('You need to include a password');
+		return false;
+	}  else {
+		return true;
+	}
 });
