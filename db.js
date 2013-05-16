67246bbf2b6b4c0ba58707e99b85cec29c8fb3c2
Brendan McNamara <brendan@streampiece.com>
2013-05-15 22:27:42 -0500

    Replaces db variable in remove method with mongoclient instance

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ var dbPort = mongodb.Connection.DEFAULT_PORT;
 
 var newsCol = 'news';
 
-var _db = new Db(dbName, new Server('localhost', dbPort), {safe: true}); //what is safe?
+//var _db = new Db(dbName, new Server('localhost', dbPort), {safe: true}); //what is safe?
 
 //related to the news collection
 exports.newsCollection = function(callback) {
@@ -41,19 +41,25 @@ exports.removeNewsElementWithID = function(mongoID, callback) {
 	
 	//NOTE: db.close() should be nested inside the callback functions
 	//so that the database does not close before operations are complete!!!
-	_db.open(function(err, db) {
-		
+
+	var mongoclient = new MongoClient(new Server("localhost", 27017));
+	mongoclient.open(function(err, mongoclient) {
+
+		var db = mongoclient.db(dbName);
 		var parsedID = new ObjectID.createFromHexString(mongoID);
 		db.collection(newsCol, function(err, collection) {
 			
 			collection.remove({_id : parsedID}, {w: 1}, function(err, numberRemoved) {
 				
+				if (err) {
+					callback(err, null);
+				}
 				if (numberRemoved === 1) {
 					callback(err, mongoID);
 				} else {
-					callback(err, null);
+					callback(new Error("Did not remove 1 element from the database"), null);
 				}
-				db.close();
+				mongoclient.close();
 			});
 		});
 	});
