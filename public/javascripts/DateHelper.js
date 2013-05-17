6a9edc8f1bf55efe63984505095afac86c479424
Brendan McNamara <brendan@streampiece.com>
2013-05-17 18:59:51 -0500

    Implements dateHelper methods

--- public/javascripts/DateHelper.js
+++ public/javascripts/DateHelper.js
@@ -1,2 +1,46 @@
 //DateHelper is a utility class
 var DateHelper = {};
+
+DateHelper.addWeekToDate = function(date) {
+	date.setDate(date.getDate() + 7);
+}
+
+
+//returns the week day of the date as a string
+DateHelper.weekDayAsString = function(date) {
+	switch(date.getDay()) {
+		case 0: return "Sunday";
+		case 1: return "Monday";
+		case 2: return "Tuesday";
+		case 3: return "Wednesday";
+		case 4: return "Thursday";
+		case 5: return "Friday";
+
+	}
+	return "Saturday";
+}
+
+//sets the date so that the date is the same
+//with the exception of the time, which is 
+//12:00:00 am
+DateHelper.dateWithEmptyTime = function(date) {
+	date.setHours(0);
+	date.setMinutes(0);
+	date.setSeconds(0);
+	date.setMilliseconds(0);
+}
+
+//returns true if date1 is earlier than date2
+DateHelper.earlierDate = function(date1, date2) {
+	return date1.getTime() < date2.getTime();
+}
+
+
+//this function set the dateToSet to a date that is on or after the comparison date
+//but of the same week day as the dateToSet currently is
+
+DateHelper.setDateToWeekDayAfterDate = function(dateToSet, comparisonDate) {
+	while (DateHelper.earlierDate(dateToSet, comparisonDate)) {
+		DateHelper.addWeekToDate(dateToSet);
+	};
+}
--- public/javascripts/GFModel.js
+++ public/javascripts/GFModel.js
@@ -3,20 +3,20 @@
 window.FitnessClass = Backbone.Model.extend({
 
 	//time in military time, 1200 is 12 pm, 1330, is 1:30 pm
-	startTime = 0, 
-	endTime = 0,
-	instructor = '',
-	className = '',
+	startTime: 0, 
+	endTime: 0,
+	instructor: '',
+	className: '',
 	//0-based index of the day of the week that this class is in
-	dayOfWeek = 0,
+	dayOfWeek: 0,
 	//dates are strings in the form MM/DD/YYYY 
 	//maybe use the date class instead
 
 	//if the class is a 1-time occurence, then 
 	//the start and end dates are the same
 	//if the class keeps going, then the end date is blank
-	startDate = '',
-	endDate = '',
+	startDate: '',
+	endDate: '',
 
 
 });
