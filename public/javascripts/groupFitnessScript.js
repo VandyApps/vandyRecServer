296f725f6b41fe47f6ad74c284a32cb8f2744214
Brendan McNamara <brendan@streampiece.com>
2013-05-17 21:09:54 -0500

    Fixes call to jquery color

--- public/javascripts/groupFitnessScript.js
+++ public/javascripts/groupFitnessScript.js
@@ -1,11 +1,13 @@
 //animate color changes of the blocks
-
+console.log("called script");
 $('.dayBlock').mouseover(function() {
-
-	$(this).animate({backgroundColor: 'rgba(200, 200, 200, 1)'});
+	console.log("Called mouseover");
+	$(this).animate( { backgroundColor: 'rgba(200, 200, 200, 1)' } );
+	console.log("Animated");
 });
 
 $('.dayBlock').mouseout(function() {
-
-	$(this).animate({backgroundColor: 'rgba(0, 0, 0, 0)'});
+	console.log("called mouseout");
+	$(this).animate( { backgroundColor: 'rgba(0, 0, 0, 0)' } );
+	console.log("animated");
 });
