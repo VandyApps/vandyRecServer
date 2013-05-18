5be942e8dcfb19c3b3952fec0a683ee92689542d
Brendan McNamara <brendan@streampiece.com>
2013-05-17 21:14:53 -0500

    Removes error checking code

--- public/javascripts/groupFitnessScript.js
+++ public/javascripts/groupFitnessScript.js
@@ -1,13 +1,11 @@
 //animate color changes of the blocks
-console.log("called script");
 $('.dayBlock').mouseover(function() {
-	console.log("Called mouseover");
 	$(this).animate( { backgroundColor: 'rgba(200, 200, 200, 1)' } );
-	console.log("Animated");
+	
 });
 
 $('.dayBlock').mouseout(function() {
-	console.log("called mouseout");
+	
 	$(this).animate( { backgroundColor: 'rgba(0, 0, 0, 0)' } );
-	console.log("animated");
+	
 });
