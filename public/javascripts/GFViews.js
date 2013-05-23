bfa8a1f38ce322e369ea3767baac196cd4d004f3
Brendan McNamara <brendan@streampiece.com>
2013-05-23 18:46:58 -0500

    Adds log message to rendering of calendar

--- public/javascripts/GFViews.js
+++ public/javascripts/GFViews.js
@@ -172,7 +172,7 @@ var MonthView = Backbone.View.extend({
 		
 	},
 	render: function() {
-		
+		console.log("render was called");
 		//set the display to the month and year indication outside of
 		//calendar element
 		//month and year index tags are used for holding data that is not 
