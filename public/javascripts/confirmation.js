4d8bfc2a494204135e21d2cdaceed604f5086980
Brendan McNamara <brendan@streampiece.com>
2013-05-24 00:05:36 -0500

    Finishes creating confirmation box tag elements and styling

--- public/javascripts/confirmation.js
+++ public/javascripts/confirmation.js
@@ -2,14 +2,29 @@
 var ConfirmationBox = Backbone.View.extend({
 
 	message: '',
+	button1Name: '',
+	button2Name: '',
 	initialize: function(options) {
 		this.message = options.message;
+		this.button1Name = options.button1Name;
+		this.button2Name = options.button2Name;
 		this.render();
 	},
 	render: function() {
 		this.$el = $('<div id="confirmationBox"></div>');
-		this.$el.append('<div id="message" style='+this.getMessageStyle()+'>'+this.message+'</div>');
+
+		this.$el.append('<div id="message">'+this.message+'</div>');
+		this.$el.append('<div id="inputs"></div>');
+		this.$el.children('#inputs').append('<input id="button1" type="button" />');
+		this.$el.children('#inputs').children('#button1').val(this.button1Name);
+		this.$el.children('#inputs').append('<input id="button2" type="button" />');
+		this.$el.children('#inputs').children('#button2').val(this.button2Name);
+
 		this.$el.attr('style', this.getBoxStyle());
+		$('#message', this.$el).attr('style', this.getMessageStyle());
+		this.$el.children('#inputs').attr('style', this.getButtonWrapperStyle());
+		this.$el.children('#inputs').children('#button1').attr('style', this.getButtonStyle());
+		this.$el.children('#inputs').children('#button2').attr('style', this.getButtonStyle());
 		$('body').append(this.$el);
 	},
 	show: function(animated) {
@@ -25,7 +40,7 @@ var ConfirmationBox = Backbone.View.extend({
 		return "display: block; position: absolute; top: 20px; width: 250px; left: 25px; height: 100px; text-align: center;";
 	},
 	getButtonStyle: function() {
-		return "margin: 0px 15px; width: 60px; height: 30px; border: solid black 1px; border-radius: 10px;"
+		return "margin: 0px 15px; width: 60px; height: 30px; border: solid black 1px; border-radius: 10px; cursor: pointer;"
 	},
 	getButtonWrapperStyle: function() {
 		return "display: block; position: absolute; top: 125px; text-align: center; width: 200px; left: 50px;"
