b84eee940190d409a2c7c009f0877d1d0747d4d6
Brendan McNamara <brendan@streampiece.com>
2013-05-25 13:43:13 -0500

    Creates loading animation

--- public/javascripts/loadMessage.js
+++ public/javascripts/loadMessage.js
@@ -2,6 +2,9 @@
 //the styling for this script is in a css stylesheet for quick rendering
 var LoadMessage = Backbone.View.extend({
 	deleteWhenDone: false,
+	timeLoop: null,
+	numberOfDots: 0,
+
 	initialize: function(options) {
 		
 		if (typeof option !== 'undefined' && typeof options.deleteWhenDone !== 'undefined') {
@@ -27,9 +30,23 @@ var LoadMessage = Backbone.View.extend({
 
 	},
 	start: function() {
+		this.$el.show();
+		var self = this;
+		var showProgress = function() {
+			
+			self.numberOfDots = (self.numberOfDots + 1) % 10;
+			var progressElement = '';
+			for (var i = 0; i < self.numberOfDots; ++i) {
+				progressElement = progressElement + '.';
+			}
+			$('#loadProgress').text(progressElement);
+		}
+		
+		this.timeLoop = setInterval(showProgress, 100);
 
 	},
 	stop: function() {
-
+		clearInterval(this.timeLoop);
+		this.$el.hide();
 	}
 });
