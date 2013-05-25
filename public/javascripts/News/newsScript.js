28d457764558a71770d58975b60ec20dff6814b4
Brendan McNamara <brendan@streampiece.com>
2013-05-25 12:00:46 -0500

    Removes animation during reset events

--- public/javascripts/News/newsScript.js
+++ public/javascripts/News/newsScript.js
@@ -1,6 +1,8 @@
 //make sure that the reset event is only being called during server syncing
+//animation is turned off during reset events
 eventCollection.on('reset', function() {
-	
+	var currentAnimationValue = tableView.animate;
+	tableView.animate = false;
 	//must remove all elements from the table view first
 	tableView.removeAllElements();
 	//make sure the elements are in the correct order by priority number
@@ -11,7 +13,7 @@ eventCollection.on('reset', function() {
 	eventCollection.models.forEach(function(model) {
 		var newElement = new NewsView.NewsEventView({model: model, appendToTableView: true});
 	});
-
+	tableView.animate = currentAnimationValue;
 });
 
 
