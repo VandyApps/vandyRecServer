06804c1aaf5394f55b4cf6978c65bee9237313d5
Brendan McNamara <brendan@streampiece.com>
2013-05-31 18:09:18 -0500

    Renders name of special date within calendar block

--- public/javascripts/GF/GFViews.js
+++ public/javascripts/GF/GFViews.js
@@ -53,8 +53,10 @@ GFView.BlockView = Backbone.View.extend({
 
 		if (typeof this.specialDate !== 'undefined') {
 			this.$el.addClass('specialDay');
+			this.$el.append('<div class="specialDayIndicator">'+this.specialDate.getTitle()+'</div>');
 		} else {
 			this.$el.removeClass('specialDay');
+			$('.specialDayIndicator', this.$el).remove();
 		}
 
 		this.$el.append('<div class="dayIndicator">'+this.day+'</div>');
@@ -102,9 +104,11 @@ GFView.BlockView = Backbone.View.extend({
 		}
 
 		if (typeof this.specialDate !== 'undefined') {
+			this.$el.append('<div class="specialDayIndicator">'+this.specialDate.getTitle()+'</div>');
 			this.$el.addClass('specialDay');
 		} else {
 			this.$el.removeClass('specialDay');
+			$('.specialDayIndicator', this.$el).remove();
 		}
 
 		this.$('.dayIndicator').text(this.day.toString());
