a4ab7ff930d66ec5373f0f33685cd0d6054586b7
Brendan McNamara <brendan@streampiece.com>
2013-06-15 11:55:42 -0500

    implements methods for HoursTable separate sort into sort and reload

--- public/javascripts/Hours/HoursView.js
+++ public/javascripts/Hours/HoursView.js
@@ -51,27 +51,58 @@ HoursView.HoursTable = Backbone.View.extend({
         //section header above the table element
         return this.$el.prev().text();
     },
+    //the sort method sorts the array of views
+    //but does not render the new sorted order
     sort: function() {
-        //remove all elements inside the table view
-        //this method is reloading the table view
-        this.$el.children().remove();
-
         //sort the elements based on their sort values 
         //elements are in ascending sortValue order
         this.views.sort(function(view1, view2) {
             return view2.getSortValue() - view1.getSortValue();
         });
+    },
+    //reloads the views within the views array
+    //to display in the order they are in the array
+    reload: function() {
+        //remove all elements inside the table view
+        //this method is reloading the table view
+        this.$el.children().remove();
 
         //rerender all the elements within the view
         this.views.forEach(function(view) {
             this.$el.append(view.$el);
         }, this);
-    },
+    }
+    //adds an HoursItem view to the end of the list and renders the item
     push: function(hoursItem) {
-
+        this.views.push(hoursItem);
+        this.$el.append(hoursItem.$el);
     },
+    //this method adds a view to the correct slot within the list
+    //if the list is not sorted before this method is
+    //called, the list is sorted before adding the new element
     add: function(hoursItem) {
-
+        if (this.isSorted()) {
+            var itemAdded = false;
+            for (var i = 0; i < this.views; ++i) {
+                if (hoursItem.getSortValue() < this.views[i].getSortValue()) {
+                    itemAdded = true;
+                    this.views.slice(i, 0, hoursItem);
+                }
+            }
+        } else {
+            this.views.push(hoursItem);
+            this.sort();
+            this.reload();
+        }
+    },
+    //checks if the list is sorted
+    isSorted: function() {
+        for (var i = 1; i < this.views.length; ++i) {
+            if (this.views[i].getSortValue() < this.views[i-1].getSortValue()) {
+                return false;
+            }
+        }
+        return true;
     }
 
 });
