c616443ebf7cfcfd73b2366610bf5e58319a21a9
Brendan McNamara <brendan@streampiece.com>
2013-06-18 18:10:30 -0500

    Adds more demo data to script

--- public/javascripts/Hours/HoursScript.js
+++ public/javascripts/Hours/HoursScript.js
@@ -1,6 +1,6 @@
 //(function(global) {
 var global = this;
-    var hoursCollection = new global.HoursModel.HoursCollection();
+    
 
     var model1 = new HoursModel.Hours(
         {
@@ -52,11 +52,96 @@ var global = this;
                 {startTime: '06:00am', endTime: '06:00pm'}
             ]
         });
+
+
+    var model4 = new HoursModel.Hours(
+        {
+            name: 'Pool Hours', 
+            priorityNumber: 1, 
+            startDate: '01/08/2013',
+            endDate: '01/08/2014',
+            facilityHours: false,
+            closedHours: false,
+            times: 
+            [
+                {startTime: '08:30am', endTime: '12:00pm'},
+                {startTime: '04:30am', endTime: '12:00pm'},
+                {startTime: '12:00am', endTime: '09:45pm'},
+                {startTime: '06:00am', endTime: '06:00pm'}
+            ]
+        });
+    var model5 = new HoursModel.Hours(
+        {
+            name: 'Thanksgiving', 
+            priorityNumber: 1, 
+            startDate: '09/08/2013',
+            endDate: '09/17/2013',
+            facilityHours: true,
+            closedHours: true,
+            times: 
+            [
+                {startTime: '08:30am', endTime: '12:00pm'},
+                {startTime: '04:30am', endTime: '12:00pm'},
+                {startTime: '12:00am', endTime: '09:45pm'},
+                {startTime: '06:00am', endTime: '06:00pm'}
+            ]
+        });
+    var model6 = new HoursModel.Hours(
+        {
+            name: 'Christmas', 
+            priorityNumber: 2, 
+            startDate: '12/25/2013',
+            endDate: '12/25/2013',
+            facilityHours: true,
+            closedHours: true,
+            times: 
+            [
+                {startTime: '08:30am', endTime: '12:00pm'},
+                {startTime: '04:30am', endTime: '12:00pm'},
+                {startTime: '12:00am', endTime: '09:45pm'},
+                {startTime: '06:00am', endTime: '06:00pm'}
+            ]
+        });
+
+    var model7 = new HoursModel.Hours(
+        {
+            name: 'New Hours', 
+            priorityNumber: 2, 
+            startDate: '02/12/2013',
+            endDate: '4/12/2013',
+            facilityHours: true,
+            closedHours: false,
+            times: 
+            [
+                {startTime: '08:30am', endTime: '12:00pm'},
+                {startTime: '04:30am', endTime: '12:00pm'},
+                {startTime: '12:00am', endTime: '09:45pm'},
+                {startTime: '06:00am', endTime: '06:00pm'}
+            ]
+        });
+
+    var model8 = new HoursModel.Hours(
+        {
+            name: 'Contruction Hours', 
+            priorityNumber: 2, 
+            startDate: '10/12/2013',
+            endDate: '09/18/2014',
+            facilityHours: true,
+            closedHours: false,
+            times: 
+            [
+                {startTime: '08:30am', endTime: '12:00pm'},
+                {startTime: '04:30am', endTime: '12:00pm'},
+                {startTime: '12:00am', endTime: '09:45pm'},
+                {startTime: '06:00am', endTime: '06:00pm'}
+            ]
+        });
     //global.hoursCollection.add(model1);
 
     var hoursItem1 = new global.HoursView.HoursItem({model: model1});
     var hoursItem2 = new global.HoursView.HoursItem({model: model2});
     var hoursItem3 = new global.HoursView.HoursItem({model: model3});
+    var hoursCollection = new global.HoursModel.HoursCollection([model1, model2, model3, model4, model5, model6, model7, model8]);
     var hoursTableView = new global.HoursView.HoursTable();
     hoursTableView.add(hoursItem1);
     hoursTableView.add(hoursItem2);
