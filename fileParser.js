b3da09e25aafa43bee4e8fc8d5160b04349a3f3e
Brendan McNamara <brendan@streampiece.com>
2013-07-15 15:48:24 -0500

    Adds parseHTML test method to fileParser

--- fileParser.js
+++ fileParser.js
@@ -1,3 +1,10 @@
 var jsdom = require('jsdom');
 
-console.log("Here is file parser");
+exports.parseHTML = function(html, callback) {
+	var document = jsdom.jsdom(html),
+	    window = document.createWindow;
+
+	jsdom.jQuerify(window, './public/javascripts/jQuery-ui/js/jquery-1.9.1.js', function() {
+		console.log(window.$('body').html());
+	});
+};
