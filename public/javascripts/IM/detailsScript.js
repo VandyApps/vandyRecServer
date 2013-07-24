e95d3b8c0f0d518cc104d41ed8ed31bda14fdebe
Brendan McNamara <brendan@streampiece.com>
2013-07-23 22:21:35 -0500

    Adds object definitions for the sportsDetails script, not yet implemented to work

---
+++ public/javascripts/IM/detailsScript.js
@@ -0,0 +1,101 @@
+//Declarations
+var sportDetails,
+	E_DatesView,
+	S_DatesView,
+	TeamsView,
+	GamesView,
+	EditView;
+
+
+
+//Model
+
+SportDetails = Backbone.Model.extend({
+
+});
+
+//singleton
+SportDetails.getInstance = function() {
+	if (!sportDetails.instance) {
+		sportDetails.instance = new SportDetails();
+	}
+	return sportDetails.instance;
+};
+
+//Views
+
+E_DatesView = Backbone.View.extend({
+	el: '#entryDates'
+	startDate: '',
+	endDate: '',
+	isHidden: true,
+	events: {
+
+	},
+	render: function() {
+
+	},
+	toggle: function() {
+
+	},
+	show: function() {
+
+	},
+	hide: function() {
+
+	}
+});
+
+E_DatesView.getInstance = function() {
+	if (!this.instance) {
+		this.instance = new E_DatesView();
+	}
+	return this.instance;
+};
+
+
+S_DatesView = Backbone.View.extend({
+	el: '#seasonDates'
+});
+
+S_DatesView.getInstance = function() {
+	if (!this.instance) {
+		this.instance = new S_DatesView();
+	}
+	return this.instance;
+};
+
+
+TeamsView = Backbone.View.extend({
+	el: '#teams'
+});
+
+TeamsView.getInstance = function() {
+	if (!this.instance) {
+		this.instance = new TeamsView();
+	}
+	return this.instance;
+};
+
+GamesView = Backbone.View.extend({
+	el: '#games',
+});
+
+GamesView.getInstance = function() {
+	if (!this.instance) {
+		this.instance = new GamesView();
+	}
+	return this.instance;
+};
+
+//edit view contains all the windows that have
+//access to editting the data within the model
+//each window has a singleton that can be accessed through
+//this method
+EditView = (function() {
+	
+	return {
+
+	};
+
+});
