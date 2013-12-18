4968ce9170e69e2903ab5db44830a0da4f498421
Brendan McNamara <bahai9@live.com>
2013-12-18 10:14:01 -0800

    Implements singleton view and adds a few instance methods for view

--- public/javascripts/IM/IM.view.js
+++ public/javascripts/IM/IM.view.js
@@ -1,5 +1,43 @@
 //Intramurals should be defined within model
 Intramurals.View = {};
-Intramurals.View.Categories = Backbone.View.extend({
+Intramurals.View.Categories = Backbone.View.extend(
+{
+	el: '#intramurals',
+	initialize: function(options) {
+		//collection should be of type Intramurals.Model.Categories
+		//it is the views job to sync the collection
+		
+		this.collection = null || (options && options.collection);
+		if (this.collection) {
+			this.collection.on('sync', this.onCollectionSync.bind(this));
+			this.collection.fetch();
+		}
 
+		
+	},
+
+	generateList: function() {
+		
+	},
+	clearList: function() {
+		this.$el.html('<li>Fall</li><ul></ul><li>Winter</li><ul></ul><li>Spring</li><ul></ul><li>Summer</li><ul></ul>');
+	},
+	
+
+
+	onCollectionSync: function() {
+		this.generateList();
+	}
+},
+{
+	instance: null,
+	getInstance: function() {
+		if (!Intramurals.View.Categories.instance) {
+			Intramurals.View.Categories.instance = new Intramurals.View.Categories(
+				{
+					collection: new Intramurals.Model.Categories()
+				});
+		}
+		return Intramurals.View.Categories.instance;
+	}
 });
